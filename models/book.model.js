b8f9e3d041c72512919bbaa180c32132f0a7ab95
alexi <alexi@HP-ALEXIS_ZZ>
2025-10-17 19:53:37 -0600

    feat: implementar CRUD de usuarios, ademas de crear el modelo de User y definir las rutas para realizar las operaciones de usuario

---
+++ models/book.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
